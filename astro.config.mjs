50abb89b95a1b7a482e99471c29ece57b4283bbe
hima2oo4 <himapolamarasetty2004@outlook.com>
2025-09-01 19:01:10 +0530

    fixed image reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://hima2oo4.github.io',
-    base: '/hima2oo4.github.io',
+    // base: '/dist',
     // updated build
     vite: {
         plugins: [tailwindcss()],
